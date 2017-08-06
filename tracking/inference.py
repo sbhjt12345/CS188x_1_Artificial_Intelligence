55ed61b0ab4b960654761fde3470b4f506f20764
Jiantao Hu <jh2468@cornell.edu>
2017-08-06 18:00:36 +0900

    finish problem 1

--- tracking/inference.py
+++ tracking/inference.py
@@ -149,7 +149,7 @@ class ExactInference(InferenceModule):
         pacmanPosition = gameState.getPacmanPosition()
 
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        
 
         # Replace this code with a correct observation update
         # Be sure to handle the "jail" edge case where the ghost is eaten
@@ -158,7 +158,11 @@ class ExactInference(InferenceModule):
         for p in self.legalPositions:
             trueDistance = util.manhattanDistance(p, pacmanPosition)
             if emissionModel[trueDistance] > 0:
-                allPossible[p] = 1.0
+                allPossible[p] = self.beliefs[p] * emissionModel[trueDistance]
+
+        if noisyDistance is None:
+            allPossible = util.Counter()
+            allPossible[self.getJailPosition()] = 1.0
 
         "*** END YOUR CODE HERE ***"
 
